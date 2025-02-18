febbda7fafdb5e3ac5c088701a2fbdf2470dd492
xl-9527 <1824670705@qq.com>
2025-02-18 17:04:36 +0800

    feat(roctet-mq): 添加 RocketMqDemo 类
    - 在 rockct-mq-simple 模块中创建了 RocketMqDemo 类
    - 添加了一个简单的 main 方法，打印 "Hello, World!"

---
+++ roctet-mq/rockct-mq-simple/src/main/java/com/mq/rocket/RocketMqDemo.java
@@ -0,0 +1,11 @@
+package com.mq.rocket;
+
+/**
+ * @author xl-9527
+ * @since 2025/2/18
+ **/
+public class RocketMqDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
