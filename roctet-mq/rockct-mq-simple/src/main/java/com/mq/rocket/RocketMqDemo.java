4e4ead9af7af142711684fd8a2efd5fd06313705
xl-9527 <1824670705@qq.com>
2025-02-23 18:32:22 +0800

    feat(rocket-mq-simple): 添加 RocketMQ 消息发送功能- 在 pom.xml 中添加 RocketMQ 客户端依赖 - 新增 RocketMqDemo 类，实现消息发送功能 - 添加消息构建和发送方法 - 引入事务消息钩子

--- roctet-mq/rockct-mq-simple/src/main/java/com/mq/rocket/RocketMqDemo.java
+++ roctet-mq/rockct-mq-simple/src/main/java/com/mq/rocket/RocketMqDemo.java
@@ -1,11 +1,36 @@
 package com.mq.rocket;
 
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+
+import java.nio.charset.StandardCharsets;
+
 /**
  * @author xl-9527
  * @since 2025/2/18
  **/
 public class RocketMqDemo {
+
+    private final static String TOPIC = "test";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    }
+
+    private ClientServiceProvider getProductServer() {
+        return ClientServiceProvider.loadService();
+    }
+
+    private Message builtMessage(ClientServiceProvider client, String msg) {
+        return client.newMessageBuilder()
+                .setBody(msg.getBytes(StandardCharsets.UTF_8))
+                .setTopic(TOPIC)
+                .build();
+    }
+
+    private void sendMessage(ClientServiceProvider client, Message msg) {
+        client.getProducerBuilder()
+                .setTransactionalMessageHook(new TransactionalMessageHook())
+                .build()
+                .send(msg);
     }
 }
